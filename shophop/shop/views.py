345769c951fa9adfce98c88c3cf4674b5845bb6c
amarsarkar007 <2016sarkaramar@gmail.com>
2021-12-10 10:26:43 +0530

    initial commit

---
+++ shophop/shop/views.py
@@ -0,0 +1,147 @@
+from django.db.models import query
+from django.shortcuts import render
+from django.http import HttpResponse, response
+from .models import Product, Contact, Orders, OrderUpdate
+from math import ceil
+import json
+from django.views.decorators.csrf import csrf_exempt
+from PayTm import Checksum
+MERCHANT_KEY = 'Your-Merchant-Key-Here'
+
+# Create your views here.
+
+def index(request):
+    # products = Product.objects.all()
+    # print(products)
+    # n = len(products)
+    # nslides = n//4 + ceil((n/4)-(n//4))
+    # allprods = [[products, range(1, nslides), nslides], [products, range(1, nslides), nslides]]
+
+    allprods = []
+    catprods = Product.objects.values('category','id')
+    cats = {item['category'] for item in catprods}
+    for cat in cats:
+        prod = Product.objects.filter(category=cat)
+        n = len(prod)
+        nslides = n//4 + ceil((n/4)-(n//4))
+        allprods.append([prod, range(1,nslides), nslides])
+
+    params = {'allprods':allprods}
+    return render(request, 'shop/index.html', params)
+
+def searchMatch(query, item):
+    '''return true only if the query matches the item'''
+    if query in item.desc.lower() or query in item.product_name.lower() or query in item.category.lower():
+        return True
+    else:
+        return False
+
+def search(request):
+    query = request.GET.get('search')
+    allprods = []
+    catprods = Product.objects.values('category','id')
+    cats = {item['category'] for item in catprods}
+    for cat in cats:
+        prodtemp = Product.objects.filter(category=cat)
+        prod = [item for item in prodtemp if searchMatch(query, item)]
+
+        n = len(prod)
+        nslides = n//4 + ceil((n/4)-(n//4))
+        if len(prod) != 0:
+            allprods.append([prod, range(1,nslides), nslides])
+
+    params = {'allprods':allprods, "msg" : ""}
+    if len(allprods) == 0:
+        params = {'msg': "Sorry, No Result Found !"}
+    return render(request, 'shop/search.html', params)
+
+def about(request):
+    return render(request, 'shop/about.html')
+
+def contact(request):
+    thank = False
+    if request.method == 'POST':
+        # print(request)
+        name = request.POST.get('name','')
+        email = request.POST.get('email','')
+        phone = request.POST.get('phone','')
+        desc = request.POST.get('desc','')
+        # print(name, email, phone, desc)
+        contact= Contact(name=name, email=email, phone=phone, desc=desc)
+        contact.save()
+        thank = True
+    return render(request, 'shop/contact.html', {'thank':thank})
+
+def tracker(request):
+    if request.method=="POST":
+        orderId = request.POST.get('orderId', '')
+        email = request.POST.get('email', '')
+        try:
+            order = Orders.objects.filter(order_id=orderId, email=email)
+            if len(order)>0:
+                update = OrderUpdate.objects.filter(order_id=orderId)
+                updates = []
+                for item in update:
+                    updates.append({'text': item.update_desc, 'time': item.timestamp})
+                    response = json.dumps({"status":"success", "updates": updates, "itemsJson": order[0].items_json}, default=str)
+                return HttpResponse(response)
+            else:
+                return HttpResponse('{"status":"noitem"}')
+        except Exception as e:
+            return HttpResponse('{"status":"error"}')
+
+    return render(request, 'shop/tracker.html')
+
+
+def productview(request, myid):
+    #Fetch the product using id
+    prod = Product.objects.filter(id=myid)
+    # print(prod)
+
+    return render(request, 'shop/prodview.html',{'product':prod[0]})
+
+def checkout(request):
+    if request.method == 'POST':
+        # print(request)
+        items_json = request.POST.get('itemsJson','')
+        name = request.POST.get('name','')
+        amount = request.POST.get('amount','')
+        email = request.POST.get('email','')
+        address = request.POST.get('address1','')+ " " + request.POST.get('address2','')
+        city = request.POST.get('city','')
+        state = request.POST.get('state','')
+        zip_code = request.POST.get('zip_code','')
+        phone = request.POST.get('phone','')
+        # print(name, email, phone, desc)
+        order= Orders(items_json=items_json, name=name, email=email, address=address, city=city, state=state, zip_code= zip_code, phone=phone, amount=amount)
+        order.save()
+        update = OrderUpdate(order_id=order.order_id, update_desc="The order has been placed")
+        update.save()
+        thank = True
+        id = order.order_id
+        # return render(request, 'shop/checkout.html', {'thank':thank, 'id':id})
+        #Requst PayTm to transfer the amount to your account after payment by user.
+        param_dict={
+
+            'MID': 'Your-Merchant-Id-Here',
+            'ORDER_ID': str(order.order_id),
+            'TXN_AMOUNT': str(amount),
+            'CUST_ID': email,
+            'INDUSTRY_TYPE_ID': 'Retail',
+            'WEBSITE': 'WEBSTAGING',
+            'CHANNEL_ID': 'WEB',
+            'CALLBACK_URL':'http://127.0.0.1:8000/shop/handlerequest/',
+
+        }
+
+        return render(request, 'shop/paytm.html',{'param_dict': param_dict})
+    return render(request, 'shop/checkout.html')
+
+
+@csrf_exempt
+def handlerequest(request):
+    return HttpResponse("Done")
+    #paytm will send you post request here.
+    pass
+
+
